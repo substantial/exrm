8c289a84681674d1b2a00d9888da259e2c4a85ed
Aaron Jensen <aaronjensen@gmail.com>
2016-01-25 12:53:55 -0800

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ReleaseManager.Mixfile do
 
   def project do
     [ app: :exrm,
-      version: "1.0.0-rc7",
+      version: "1.0.0-rc7-2",
       elixir: "~> 1.0",
       description: description,
       package: package,
